fe5587dce7adc828e123471096e3bf01ffbdd410
Douglas Kim <dougkim@15solutions.com>
2022-05-26 10:55:48 -1000

    Implement database migration to create initial set of roles.

---
+++ sources/webapp/database/migrations/2022_05_26_204259_add_roles.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    private $roleNames = [
+        'super-admin',
+        'admin',
+        'super-editor',
+        'editor',
+        'author',
+        'commentator',
+        'reviewer',
+        'proofreader',
+        'publisher'
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->roleNames as $roleName) {
+            Role::create(['name' => $roleName]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Role::whereIn('name', $this->roleNames)->delete();
+    }
+};
