15eada4909a448ac356dbf159b9d34e4e49e7685
Roger Klein <rklein@klik-info.ch>
2022-05-30 17:33:38 +0200

    Create inertia page with simple listing of Document tree

---
+++ sources/webapp/resources/js/Pages/DocumentTree.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    documentTree: Array
+});
+</script>
+
+<template>
+    <AppLayout title="Document Tree">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Document Tree
+            </h2>
+        </template>
+        <div class="ml-16">
+        <ul v-for="node in documentTree" :key="node.id">
+
+        <li>{{node.label_de}}</li>
+        
+        </ul>
+        </div>
+       
+    </AppLayout>
+</template>
