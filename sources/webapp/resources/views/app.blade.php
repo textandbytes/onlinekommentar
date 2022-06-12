6fb27175e7d43db6e64c5b43272425cc8ab3e4d2
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-06-12 15:32:13 +0200

    Javascript code splitting: Use extract() method in Laravel mix to extract vendor code into separate bundle file for more efficient caching

--- sources/webapp/resources/views/app.blade.php
+++ sources/webapp/resources/views/app.blade.php
@@ -14,6 +14,8 @@
 
         <!-- Scripts -->
         @routes
+        <script src="{{ mix('js/manifest.js') }}" defer></script>
+        <script src="{{ mix('js/vendor.js') }}" defer></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
         @inertiaHead
     </head>
--- sources/webapp/webpack.mix.js
+++ sources/webapp/webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix
+    .js('resources/js/app.js', 'public/js')
+    .extract()
+    .vue(3)
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
