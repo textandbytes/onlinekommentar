da0579dbd0b1bce5debf43d84975368fff41d30f
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-06-16 01:25:31 +0200

    Implement separate layouts for CMS and fronten. Todo: fix swapping layouts on redirects (login, logut)

--- sources/webapp/app/Http/Middleware/HandleInertiaRequests.php
+++ sources/webapp/app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'is_cms' => fn() => $request->is('cms/*'),
         ]);
     }
 }
--- sources/webapp/resources/js/app.js
+++ sources/webapp/resources/js/app.js
@@ -1,17 +1,20 @@
 require('./bootstrap');
 
 import { createApp, h } from 'vue';
-import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link, Head, usePage } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import Layout from './Shared/Layout.vue';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const initialPage = JSON.parse(app.dataset.page);
 
 createInertiaApp({
     title: (title) => `${appName} - ${title}`,
     resolve: async name => {
         let page = (await import(`./Pages/${name}.vue`)).default;
-        page.layout ??= Layout; // set the default layout if a layout is not defined
+        if (!initialPage.props.is_cms) {
+            page.layout ??= Layout; // set the default layout if a layout is not defined
+        }
         return page;
     },
     setup({ el, app, props, plugin }) {
