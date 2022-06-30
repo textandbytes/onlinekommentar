3af53a26c9e94f2d9e9db0384cb19a81d2b17c2c
Roger Klein <rklein@klik-info.ch>
2022-06-30 16:26:59 +0200

    Add model method for preparing the tree data recursively

--- sources/webapp/app/Models/DocumentTree.php
+++ sources/webapp/app/Models/DocumentTree.php
@@ -21,4 +21,41 @@ class DocumentTree extends Model
         return $this->hasMany(DocumentTree::class, 'parent_id');
     }
 
+   /**
+     * Prepare tree data recursively
+     *
+     * @param array  $aItems    - data to prepare
+     * @param int    $iParentId - 1 is the id of the root record
+     *
+     * @return array $aTree
+     */
+    protected function prepareTreeData(array $aItems, $iParentId = 1)
+    {
+      $aTree = [];
+
+      // Loop through the items
+      foreach ($aItems as $aItem) {
+
+        // The parent id of the item matches the current $iParentId
+        if ((int) $aItem['parent_id'] == $iParentId) {
+          
+          // Call the function recursively, use the item's id as the parent's id
+          // The function returns the list of children as an array or an empty array
+          $aChildren = $this->prepareTreeData($aItems, $aItem['id']);
+
+          // Children array is not empty
+          if (count($aChildren) > 0) {
+
+            // Store all children of the current item
+            $aItem['children'] = $aChildren;
+          }
+
+          // Fill the output
+          $aTree[] = $aItem;
+        }
+      }
+
+      return $aTree;
+    }
+
 }
