dc17e3950d88e91f262e95c70a1bd8b009006fb6
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-07-05 13:03:58 +0200

    Add link to document tree in CMS navigation

--- sources/webapp/resources/js/Layouts/AppLayout.vue
+++ sources/webapp/resources/js/Layouts/AppLayout.vue
@@ -52,6 +52,11 @@ const logout = () => {
                                     Dashboard
                                 </JetNavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <JetNavLink :href="route('tree.index')" :active="route().current('tree.index')">
+                                    Document Tree
+                                </JetNavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
