3f4a958ad0e918a531b784b890b03fd07aa13c07
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-07-09 17:38:14 +0200

    Fetch document tree from server as JSON when sidebar is mounted and display document tree and logo in sidebar when it is open

--- sources/webapp/resources/js/Shared/Sidebar.vue
+++ sources/webapp/resources/js/Shared/Sidebar.vue
@@ -2,6 +2,16 @@
   <div id="sidebar" :class="{ 'open': isSidebarOpen }" class="z-50 absolute top-0 bottom-0 left-0 w-10 h-screen bg-ok-blue rounded-tr-xl rounded-br-xl">
   <div id="shadow" class="absolute top-0 bottom-0 left-0 w-2 h-screen bg-transparent"></div>
     
+    <div v-if="isSidebarOpen" >
+      <div class="block p-4">
+        <img src="/img/ok-logo-text.svg" alt="Onlinekommentar – der frei zugängliche Rechtskommenter" class="w-48 mt-4" />
+      </div>
+
+       <div class="ml-4 mt-4">
+        <tree :tree-data="treeData"></tree>
+      </div>
+    </div>
+    
     <!-- sidebar handle -->
     <div @click="toggleSidebar" id="handle" class="absolute right-0 top-1/2 -mt-16 -mr-4 py-6 bg-ok-blue rounded-tr-lg rounded-br-lg shadow-tr-lg shadow-br-lg">
       <div id="handle-text" class="text-xs uppercase mb-6">Inhalt</div>
@@ -13,13 +23,26 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
+  import Tree from "../Shared/Tree";
 
   const isSidebarOpen = ref(false)
+  const treeData = ref({})
   
   const toggleSidebar = () => {
     isSidebarOpen.value = !isSidebarOpen.value
   }
+
+  onMounted(() => {
+    // make an axios request to get the document tree
+    axios.get('/tree')
+      .then(response => {
+        treeData.value = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
 
 </script>
 
