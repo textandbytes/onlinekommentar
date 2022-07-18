ef3572aa8419f54ac2e8b921c5b06f30108f0e5f
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-07-18 16:54:07 +0200

    Define eloquent relationships for users in Commentary model and add functions to get editors, authors and proofreaders for a Commentary

--- sources/webapp/app/Models/Commentary.php
+++ sources/webapp/app/Models/Commentary.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Commentary extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'users_x_commentaries', 'commentary_id', 'user_id');
+    }
+
+    public function editors()
+    {
+        return $this->users()->where('role_id', 2);
+    }
+
+    public function authors()
+    {
+        return $this->users()->where('role_id', 3);
+    }
+
+    public function proofreaders()
+    {
+        return $this->users()->where('role_id', 4);
+    }
+    
 }
