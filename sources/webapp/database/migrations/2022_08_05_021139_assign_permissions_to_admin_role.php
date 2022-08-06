303ac2a8b2e0e1d82181f10b17e967480ed52417
Douglas Kim <dougkim@15solutions.com>
2022-08-06 13:51:23 -0700

    Implement database migration to assign user management permissions to the 'admin' role.

---
+++ sources/webapp/database/migrations/2022_08_05_021139_assign_permissions_to_admin_role.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $role = Role::findByName('admin');
+        $role->givePermissionTo('view-users');
+        $role->givePermissionTo('create-users');
+        $role->givePermissionTo('update-users');
+        $role->givePermissionTo('delete-users');        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $role = Role::findByName('admin');
+        $role->revokePermissionTo('delete-users');
+        $role->revokePermissionTo('update-users');
+        $role->revokePermissionTo('create-users');
+        $role->revokePermissionTo('view-users');
+    }
+};
