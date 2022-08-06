81397dc60f18b73306df5ec980145b14786daa36
Douglas Kim <dougkim@15solutions.com>
2022-08-06 13:49:54 -0700

    Implement database migration to populate permissions table with permissions to view, create, update and delete users.

---
+++ sources/webapp/database/migrations/2022_08_05_015750_add_manage_users_permissions.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+
+return new class extends Migration
+{
+    private $permissions = [
+        'view-users',
+        'create-users',
+        'update-users',
+        'delete-users',
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach ($this->permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Permission::whereIn('name', $this->permissions)->delete();
+    }
+};
