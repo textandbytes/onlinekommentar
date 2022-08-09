011425e35aa91fafa705beb4dbcf9f54d8b25bf8
Douglas Kim <dougkim@15solutions.com>
2022-08-08 23:19:41 -0700

    Remove unnecessary semicolons from app.js entrypoint file.

--- sources/webapp/resources/js/app.js
+++ sources/webapp/resources/js/app.js
@@ -1,14 +1,14 @@
-require('./bootstrap');
+require('./bootstrap')
 
-import { createApp, h } from 'vue';
-import { createInertiaApp, Link, Head, usePage } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
-import CmsLayout from '@/Layouts/CmsLayout.vue';
-import Layout from './Shared/Layout.vue';
-import NavLink from './Shared/NavLink.vue';
+import { createApp, h } from 'vue'
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import CmsLayout from '@/Layouts/CmsLayout.vue'
+import Layout from './Shared/Layout.vue'
+import NavLink from './Shared/NavLink.vue'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-const initialPage = JSON.parse(app.dataset.page);
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Onlinekommentar'
+const initialPage = JSON.parse(app.dataset.page)
 
 import mitt from 'mitt'
 const emitter = mitt()
@@ -16,25 +16,25 @@ const emitter = mitt()
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: async name => {
-        let page = (await import(`./Pages/${name}.vue`)).default;
+        let page = (await import(`./Pages/${name}.vue`)).default
 
         // set the 'layout' property to null in page components to override any layout from being used 
         if (page.layout === undefined) {
             page.layout = initialPage.props.is_cms ? CmsLayout : Layout
         }
 
-        return page;
+        return page
     },
     setup({ el, app: inertiaApp, props, plugin }) {
         const app = createApp({ render: () => h(inertiaApp, props) })
             .use(plugin)
-            .component("Head", Head)
-            .component("Link", Link)
-            .component("NavLink", NavLink)
+            .component('Head', Head)
+            .component('Link', Link)
+            .component('NavLink', NavLink)
             .mixin({ methods: { route } })
         app.config.globalProperties.emitter = emitter
         app.mount(el)
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563' })
