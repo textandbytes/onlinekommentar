0afb03ad4647733fa706ce56c08cd426b6f8f69d
Douglas Kim <dougkim@15solutions.com>
2022-08-16 15:09:58 -0700

    Implement and register middleware that sets the locale on each page request.

--- sources/webapp/app/Http/Kernel.php
+++ sources/webapp/app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
+            \App\Http\Middleware\SetLocale::class,
         ],
 
         'api' => [
