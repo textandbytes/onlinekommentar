b2b5de31e9d7144be6a591c968f2117cf14023a6
Douglas Kim <dougkim@15solutions.com>
2022-08-18 13:15:10 -0700

    Update database migration to add slug and language-specific label fields to the commentaries table.
    Temporarily remove 'document_tree_id' column since it is uncertain if a commentary can only be connected to a one element in the document tree.

--- sources/webapp/database/migrations/2022_07_18_135140_create_commentaries_table.php
+++ sources/webapp/database/migrations/2022_07_18_135140_create_commentaries_table.php
@@ -15,20 +15,20 @@ return new class extends Migration
     {
         Schema::create('commentaries', function (Blueprint $table) {
             $table->id();
-            $table->unsignedSmallInteger('document_tree_id');
+            $table->string('slug')->unique()->nullable();
+            $table->string('label_de');
+            $table->string('label_en')->nullable();
+            $table->string('label_fr')->nullable();
+            $table->string('label_it')->nullable();
             $table->longtext('content_de')->nullable();
             $table->longtext('content_en')->nullable();
             $table->longtext('content_fr')->nullable();
             $table->longtext('content_it')->nullable();
+            $table->enum('original_language', ['de', 'en', 'fr', 'it'])->default('de');
             $table->text('suggested_citation_long')->nullable();
             $table->text('suggested_citation_short')->nullable();
-            $table->enum('status', ['draft', 'in_review', 'published'])->default('draft');
-            $table->enum('original_language', ['de', 'en', 'fr', 'it'])->default('de');
             $table->string('doi')->nullable();
-            $table->foreign('document_tree_id')
-                  ->references('id')
-                  ->on('document_tree')
-                  ->onDelete('cascade');
+            $table->enum('status', ['draft', 'in_review', 'published'])->default('draft');
             $table->timestamps();
         });
     }
