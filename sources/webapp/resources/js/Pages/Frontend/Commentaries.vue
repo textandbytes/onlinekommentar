4f9705ea6028b499b910b77a13b8c17983e7427a
Douglas Kim <dougkim@15solutions.com>
2022-08-26 21:10:18 -0700

    Implement separate components for commentaries list and grid views.

--- sources/webapp/resources/js/Pages/Frontend/Commentaries.vue
+++ sources/webapp/resources/js/Pages/Frontend/Commentaries.vue
@@ -34,39 +34,17 @@
       </div>
     </div>
 
-    <div class="bg-gray-800 overflow-hidden shadow divide-y divide-gray-800 sm:divide-y-0 sm:grid sm:grid-cols-3 sm:gap-px">
-      <div
-        v-for="commentary in commentaries"
-        :key="commentary.id"
-        class="flex flex-col items-center relative group transition ease-in-out delay-150 bg-white hover:bg-ok-orange p-8 cursor-pointer"
-        @click="onClick(commentary)">
-        <div class="text-xs uppercase mb-8">
-          Bundesverfassung
-        </div>
+    <CommentariesListView
+      v-if="viewMode === 'list'"
+      :commentary-groups="commentaryGroups"
+      @selected="onSelected"
+    />
 
-        <div class="flex flex-col items-center">
-          <h2 class="text-4xl text-center font-medium font-serif mb-8">
-            {{ commentary[`label_${$page.props.locale}`] }}
-          </h2>
-
-          <div class="text-sm text-center">
-            Ein Kommentar von <i>Marco Zollinger</i>
-          </div>
-
-          <div class="text-sm text-center mt-1">
-            Herausgegeben von <i>Stefan Schlegel</i> und <i>Odile Ammannn</i>
-          </div>
-        </div>
-
-        <button
-          type="button"
-          class="transition ease-in-out delay-150 inline-flex items-center px-3 py-1.5 border border-2 border-black text-xs font-medium uppercase rounded-full text-black bg-white group-hover:bg-black group-hover:text-white mt-12">
-          {{ __('view_commentary') }}
-        </button>
-      </div>
-
-      <div v-for="n in (commentaries.length % 3) + 1" class="bg-white" />
-    </div>
+    <CommentariesGridView
+      v-else-if="viewMode === 'grid'"
+      :commentaries="commentaries"
+      @selected="onSelected"
+    />
   </div>
 </template>
 
@@ -82,6 +60,8 @@
   import { computed } from 'vue'
   import { Inertia } from '@inertiajs/inertia'
   import { usePage } from '@inertiajs/inertia-vue3'
+  import CommentariesListView from './Partials/CommentariesListView'
+  import CommentariesGridView from './Partials/CommentariesGridView'
   import FlyoutMenuWithDividers from '@/Menus/FlyoutMenuWithDividers'
 
   const props = defineProps({
@@ -89,6 +69,8 @@
   })
 
   const locale = computed(() => usePage().props.value.locale)
+
+  const viewMode = 'grid'
 
   const commentaries = computed(() => props.commentaryGroups.flat())
 
@@ -102,7 +84,7 @@
 
   const activeDocument = 'Obligationenrecht (OR)'
 
-  const onClick = (commentary) => {
+  const onSelected = (commentary) => {
     Inertia.visit(route('Frontend/Commentary', {
       locale: locale.value,
       commentary: commentary
---
+++ sources/webapp/resources/js/Pages/Frontend/Partials/CommentariesGridView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-gray-800 overflow-hidden shadow divide-y divide-gray-800 sm:divide-y-0 sm:grid sm:grid-cols-3 sm:gap-px">
+    <div
+      v-for="commentary in commentaries"
+      :key="commentary.id"
+      class="flex flex-col items-center relative group transition ease-in-out delay-150 bg-white hover:bg-ok-orange p-8 cursor-pointer"
+      @click="$emit('selected', commentary)">
+      <div class="text-xs uppercase mb-8">
+        {{ commentary.document_label ?? '&nbsp;' }}
+      </div>
+
+      <div class="flex flex-col items-center">
+        <h2 class="text-4xl text-center font-medium font-serif mb-8">
+          {{ commentary.label }}
+        </h2>
+
+        <div class="text-sm text-center">
+          Ein Kommentar von <i>Marco Zollinger</i>
+        </div>
+
+        <div class="text-sm text-center mt-1">
+          Herausgegeben von <i>Stefan Schlegel</i> und <i>Odile Ammannn</i>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        class="transition ease-in-out delay-150 inline-flex items-center px-3 py-1.5 border border-2 border-black text-xs font-medium uppercase rounded-full text-black bg-white group-hover:bg-black group-hover:text-white mt-12">
+        {{ __('view_commentary') }}
+      </button>
+    </div>
+
+    <div v-for="n in (commentaries.length % 3) + 1" class="bg-white" />
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    commentaries: { type: Object, required: true }
+  })
+</script>
