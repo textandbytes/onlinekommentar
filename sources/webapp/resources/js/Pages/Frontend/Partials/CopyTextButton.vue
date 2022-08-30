c936e0e80774dd68b8855b3f7609ee246887e04d
Douglas Kim <dougkim@15solutions.com>
2022-08-30 14:20:50 -0700

    Implement copying suggested citation on clipboard.

---
+++ sources/webapp/resources/js/Pages/Frontend/Partials/CopyTextButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="uppercase cursor-pointer" @click="copy">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
+    </svg>
+    <span v-if="label" class="ml-1">
+      {{ copyLabel }}
+    </span>
+
+    <input
+      v-on:focus="onFocus"
+      ref="textToCopy"
+      type="hidden"
+      readonly
+      :value="text"
+      tabindex="-1">
+  </div>
+</template>
+
+<script setup>
+  import { ref, computed } from 'vue'
+  import { usePage } from '@inertiajs/inertia-vue3'
+
+  const props = defineProps({
+    label: { type: String, required: false, default: null },
+    text: { type: String, required: true }
+  })
+
+  const translations = computed(() => usePage().props.value.translations)
+
+  const copyLabel = ref(props.label)
+
+  const textToCopy = ref()
+
+  const onFocus = (event) => {
+    event.target.select()
+  }
+
+  const copy = () => {
+    textToCopy.value.setAttribute('type', 'text')
+    textToCopy.value.focus()
+
+    // copy the citation and update the label
+    try {
+      document.execCommand('copy')
+      copyLabel.value = translations.value.copied_citation
+    } catch (err) {
+      copyLabel.value = translations.value.copy_citation_error
+    }
+
+    textToCopy.value.setAttribute('type', 'hidden')
+
+    // reset the label to its original value
+    setTimeout(() => {
+      copyLabel.value = props.label
+    }, 2000)
+  }
+</script>
--- sources/webapp/resources/js/Pages/Frontend/Partials/SuggestedCitationsPanel.vue
+++ sources/webapp/resources/js/Pages/Frontend/Partials/SuggestedCitationsPanel.vue
@@ -8,23 +8,23 @@
       {{ commentary.suggested_citation_short }}
     </div>
 
-    <div class="flex justify-end">
-      <div class="flex items-center uppercase cursor-pointer" @click="copy">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-1">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6" />
-        </svg>
-        {{ __('copy_citation') }}
-      </div>
-    </div>
+    <CopyTextButton
+      :label="__('copy_citation')"
+      :text="citationText"
+      class="flex items-center justify-end"
+    />
   </div>
 </template>
 
 <script setup>
-  defineProps({
+  import { computed } from 'vue'
+  import CopyTextButton from '@/Pages/Frontend/Partials/CopyTextButton'
+
+  const props = defineProps({
     commentary: { type: Object, required: true }
   })
 
-  const copy = () => {
-    console.log('copying')
-  }
+  const citationText = computed(() => {
+    return `${props.commentary.suggested_citation_short}: ${window.location.href}`
+  })
 </script>
