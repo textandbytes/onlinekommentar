f2a1523465c386def643a59fe404ca06526c1b24
Douglas Kim <dougkim@15solutions.com>
2022-08-29 17:56:29 -0700

    Implement responsive design for frontend commentary detail view.

--- sources/webapp/resources/js/Pages/Frontend/Commentary.vue
+++ sources/webapp/resources/js/Pages/Frontend/Commentary.vue
@@ -3,13 +3,13 @@
     <title>Kommentar</title>
   </Head>
 
-  <div class="max-w-6xl mx-auto mb-auto bg-white overflow-hidden px-24 py-12">
-    <div class="sm:grid sm:grid-cols-3 sm:gap-px py-4 border-b-2 border-black">
+  <div class="md:max-w-6xl md:mx-auto md:mb-auto bg-white overflow-hidden px-4 md:px-24 md:py-12">
+    <div class="flex justify-between items-center md:grid md:grid-cols-3 md:gap-px py-2 md:py-4 border-b-2 border-black">
       <div class="flex items-center text-xs uppercase">
         Table of Contents
       </div>
 
-      <div class="flex items-center justify-center text-2xl font-serif">
+      <div class="md:flex items-center justify-center text-2xl font-serif hidden">
         {{ commentary.label_de }}
       </div>
 
@@ -18,12 +18,12 @@
       </div>
     </div>
 
-    <div class="flex flex-col items-center my-12 space-y-6">
+    <div class="flex flex-col items-center my-8 md:my-12 space-y-6">
       <div class="text-xs font-sans uppercase">
         Kommentar Zu
       </div>
 
-      <div class="text-6xl font-serif">
+      <div class="text-2xl md:text-6xl font-serif">
         {{ commentary.label_de }}
       </div>
 
@@ -36,17 +36,17 @@
         </p>
       </div>
 
-      <div class="text-xs uppercase">
+      <div class="flex items-center text-xs uppercase">
         Zitiervorschlag
       </div>
     </div>
 
-    <div class="flex flex-col p-8 bg-ok-orange space-y-6">
+    <div class="flex flex-col p-4 md:p-8 bg-ok-orange space-y-4 md:space-y-6">
       <div class="font-bold font-serif">
         {{ document.label_de }}
       </div>
 
-      <div v-html="document.content_de" class="space-y-6">
+      <div v-html="document.content_de" class="space-y-4 md:space-y-6">
       </div>
     </div>
 
