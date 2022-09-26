a0b17356583061552d1a46658950f05c6abb4803
Douglas Kim <dougkim@15solutions.com>
2022-09-26 09:47:48 +0300

    Switch user storage from database to filesystem.

--- sources/webapp/config/auth.php
+++ sources/webapp/config/auth.php
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'statamic',
             'model' => App\Models\User::class,
         ],
 
