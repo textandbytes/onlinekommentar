68ad1b40f6b69153c14764787ea7db7261030492
Douglas Kim <dougkim@15solutions.com>
2022-09-29 15:41:29 +0300

    Remove obsolete "spatie/laravel-permission", "spatie/laravel-sluggable" and "caseyamcl/toc" packages.
    Remove obsolete Laravel controllers and rules.
    
    Remove references to Spatie permission and Spatie sluggable in Laravel models.

--- sources/webapp/app/Models/Commentary.php
+++ sources/webapp/app/Models/Commentary.php
@@ -4,14 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Permission\Models\Role;
-use Spatie\Sluggable\HasSlug;
-use Spatie\Sluggable\SlugOptions;
 
 class Commentary extends Model
 {
     use HasFactory;
-    use HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -34,52 +30,4 @@ class Commentary extends Model
         'doi',
         'slug',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'pivot'
-    ];
-
-    public function document()
-    {
-        return $this->hasOne(DocumentTree::class, 'id', 'document_id');
-    }
-
-    public function users()
-    {
-        return $this->belongsToMany(User::class, 'users_x_commentaries', 'commentary_id', 'user_id');
-    }
-
-    public function editors()
-    {
-        return $this->users()->where('role_id', Role::where('name', 'editor')->first()->id);
-    }
-
-    public function authors()
-    {
-        return $this->users()->where('role_id', Role::where('name', 'author')->first()->id);
-    }
-
-    /**
-     * Use the slug as the route key for binding.
-     */
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
-
-    /**
-     * Get the options for generating the slug.
-     */
-    public function getSlugOptions() : SlugOptions
-    {
-        return SlugOptions::create()
-            ->generateSlugsFrom('label_de')
-            ->saveSlugsTo('slug')
-            ->doNotGenerateSlugsOnUpdate();
-    }
 }
--- sources/webapp/app/Models/User.php
+++ sources/webapp/app/Models/User.php
@@ -2,18 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory;
-    use Notifiable;
-    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -40,27 +34,4 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
-    protected $appends = [
-        'profile_photo_url',
-    ];
-
-    public function commentaries()
-    {
-        return $this->belongsToMany(Commentary::class, 'users_x_commentaries', 'user_id', 'commentary_id');
-    }
 }
