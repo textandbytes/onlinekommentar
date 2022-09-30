5460cf7a1ec6033371126ca88537ef681473f421
Douglas Kim <dougkim@15solutions.com>
2022-09-30 14:52:39 +0300

    Create boilerplate for footnotes addon using the following command:
    % php please make:addon textandbytes/footnotes

---
+++ sources/webapp/addons/textandbytes/footnotes/src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Textandbytes\Footnotes;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
