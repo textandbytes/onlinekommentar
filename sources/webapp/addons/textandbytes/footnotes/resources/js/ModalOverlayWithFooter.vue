11f4f0ae0191dd1713f6c52fdfe7f81cfb97fc8e
Douglas Kim <dougkim@15solutions.com>
2022-10-05 22:58:56 +0300

    Extract inline Tailwind classes in modal overlay component of footnote addon into classes using @apply to fix missing backdrop when opening footnote editor.

--- sources/webapp/addons/textandbytes/footnotes/resources/js/ModalOverlayWithFooter.vue
+++ sources/webapp/addons/textandbytes/footnotes/resources/js/ModalOverlayWithFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div class="modal-container" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <!--
       Background backdrop, show/hide based on modal state.
 
@@ -10,10 +10,10 @@
         From: "opacity-100"
         To: "opacity-0"
     -->
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <div class="modal-backdrop"></div>
 
-    <div class="fixed inset-0 z-10 overflow-y-auto">
-      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+    <div class="modal-panel-container">
+      <div class="modal-panel-parent">
         <!--
           Modal panel, show/hide based on modal state.
 
@@ -24,10 +24,32 @@
             From: "opacity-100 translate-y-0 sm:scale-100"
             To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         -->
-        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div class="modal-panel">
           <slot name="body" />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script lang="postcss" scoped>
+  .modal-container {
+    @apply relative z-10;
+
+    .modal-backdrop {
+      @apply fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity;
+    }
+
+    .modal-panel-container {
+      @apply fixed inset-0 z-10 overflow-y-auto;
+
+      .modal-panel-parent {
+        @apply flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0;
+
+        .modal-panel {
+          @apply relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg;
+        }
+      }
+    }
+  }
+</script>
