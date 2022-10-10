843db5b842cd0c0777d04f01892f173a99e5ac05
Rafael Schwemmer <rafael.schwemmer@textandbytes.com>
2022-10-10 14:38:10 +0200

    Redirect / route to /de

--- sources/webapp/routes/web.php
+++ sources/webapp/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+  return redirect('/de');
+});
+
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
