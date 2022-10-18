942e788e44ee11f540b3478e8f3e2307645bba94
Douglas Kim <dougkim@15solutions.com>
2022-10-18 14:54:30 +0300

    Add mitt library as global property to Vue app instance.

--- sources/webapp/resources/js/app.js
+++ sources/webapp/resources/js/app.js
@@ -1,11 +1,14 @@
 require('./bootstrap')
 
+import { createApp } from 'vue'
+import { i18nVue } from 'laravel-vue-i18n'
+
+import mitt from 'mitt'
+const emitter = mitt()
+
 import FloatingVue from 'floating-vue'
 FloatingVue.options.themes.dropdown.placement = 'top'
 FloatingVue.options.themes.dropdown.distance = 10
-
-import { createApp } from 'vue'
-import { i18nVue } from 'laravel-vue-i18n'
 
 import AppHeader from '@/components/Layouts/AppHeader.vue'
 import AppFooter from '@/components/Layouts/AppFooter.vue'
@@ -15,7 +18,7 @@ import Footnote from '@/components/Pages/Partials/Footnote.vue'
 import Authors from '@/components/Pages/Authors.vue'
 import Editors from '@/components/Pages/Editors.vue'
 
-createApp({
+const app = createApp({
   components: {
     'app-header': AppHeader,
     'app-footer': AppFooter,
@@ -26,8 +29,11 @@ createApp({
     'editors': Editors,
   }
 })
-.use(FloatingVue)
-.use(i18nVue, { 
-  resolve: (lang) => import(`../../lang/${lang}.json`) 
-})
-.mount('#app')
+
+app.config.globalProperties.emitter = emitter
+app
+  .use(FloatingVue)
+  .use(i18nVue, { 
+    resolve: (lang) => import(`../../lang/${lang}.json`) 
+  })
+  .mount('#app')
