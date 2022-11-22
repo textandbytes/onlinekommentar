1f3ff620baf7fcc4d8d236ff84f3204aaf8c3be7
Jack Sleight <jacksleight@gmail.com>
2022-11-22 11:56:11 +0000

    Tidy up

--- sources/webapp/addons/textandbytes/converter/src/Fieldtypes/Converter.php
+++ sources/webapp/addons/textandbytes/converter/src/Fieldtypes/Converter.php
@@ -6,35 +6,4 @@ use Statamic\Fields\Fieldtype;
 
 class Converter extends Fieldtype
 {
-    /**
-     * The blank/default value.
-     *
-     * @return array
-     */
-    public function defaultValue()
-    {
-        return null;
-    }
-
-    /**
-     * Pre-process the data before it gets sent to the publish page.
-     *
-     * @param mixed $data
-     * @return array|mixed
-     */
-    public function preProcess($data)
-    {
-        return $data;
-    }
-
-    /**
-     * Process the data before it gets saved.
-     *
-     * @param mixed $data
-     * @return array|mixed
-     */
-    public function process($data)
-    {
-        return $data;
-    }
 }
