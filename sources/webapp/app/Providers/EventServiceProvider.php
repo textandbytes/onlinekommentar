7dba4e4d045d0f6561ee4712ebbc56da2414e1b7
Jack Sleight <jacksleight@gmail.com>
2023-11-30 12:21:41 +0000

    Implement custom navigation caching

--- sources/webapp/app/Providers/EventServiceProvider.php
+++ sources/webapp/app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Listeners\ClearNavCache;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Statamic\Events\EntrySaved;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        EntrySaved::class => [
+            ClearNavCache::class,
         ],
     ];
 
